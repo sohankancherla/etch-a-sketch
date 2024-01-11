52803a4bd0667a338e72d46c8cf7b1e8949b9204
skancher <sohank@gmail.com>
2024-01-11 09:47:45 -0800

    Add header to html

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ for (let i=0; i<16; i++) {
         const square = document.createElement("div");
         square.style.backgroundColor = "#ffffff";
         square.style.flex = 1;
-        square.style.border = "0.5px solid darkgrey";
         column.appendChild(square);
     }
     canvas.appendChild(column);
